97484e2f985dea04bf50a564505ba9de138b8ba0
bhismaharis <bhismaharis11@gmail.com>
2024-07-09 17:43:40 +0700

    [10] Factory & Faker

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
 @section('container')
     <article class="mb-5">
         <h1>{{ $post->title }}</h1>
-        <p>By: <a href="" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+        <p>By: <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
         {!! $post->body !!}
     </article>
     <a href="/posts" class="text-decoration-none">Back to Posts</a>
